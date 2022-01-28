82d53af01e5d74c16876974d6e9d93e2ef9164b1
Malerka232 <96832517+Malerka232@users.noreply.github.com>
2022-01-28 20:26:45 +0300

    Пофиксили меню и шапку,добавили контент

---
+++ Youtube/js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.channel-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+ 
